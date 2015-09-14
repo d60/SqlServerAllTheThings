543672a846cdbccac6fe71b705c87322f6c51076
Mogens Heller Grabe <mookid8000@gmail.com>
2015-09-14 16:22:07 +0200

    updated sample

--- CirqusDemo/CirqusDemo/Program.cs
+++ CirqusDemo/CirqusDemo/Program.cs
@@ -10,26 +10,29 @@ namespace CirqusDemo
 {
     class Program
     {
+        const string MongoConnectionString = "mongodb://localhost/sqlsatt";
+        const string SqlConnectionString = "server=.\\SQLEXPRESS; database=sqlsatt; trusted_connection=true";
+
         static void Main()
         {
-            var ordersView = new MongoDbViewManager<OrdersView>("mongodb://localhost/sqlsug");
-            var orderedItemsView = new MongoDbViewManager<OrderedItemsView>("mongodb://localhost/sqlsug");
-            var regrettableItemsView = new MongoDbViewManager<RegrettableItemsView>("mongodb://localhost/sqlsug");
+            var ordersView = new MongoDbViewManager<OrdersView>(MongoConnectionString);
+            var orderedItemsView = new MongoDbViewManager<OrderedItemsView>(MongoConnectionString);
+            var regrettableItemsView = new MongoDbViewManager<RegrettableItemsView>(MongoConnectionString);
 
             var commandProcessor = CommandProcessor.With()
                 .Logging(l => l.UseConsole())
-                .EventStore(e => e.UseSqlServer("server=.;database=sqlsug;trusted_connection=true", "Events"))
+                .EventStore(e => e.UseSqlServer(SqlConnectionString, "Events"))
                 .EventDispatcher(e => e.UseViewManagerEventDispatcher(ordersView, orderedItemsView, regrettableItemsView))
                 .Create();
 
             using (commandProcessor)
             {
-                const string orderId = "order/1";
+                const string orderId = "order/5";
 
                 commandProcessor.ProcessCommand(new CreateNewOrder(orderId));
                 commandProcessor.ProcessCommand(new AddItem(orderId, "Beer", 6));
                 commandProcessor.ProcessCommand(new AddItem(orderId, "Nuts", 3));
-                commandProcessor.ProcessCommand(new RemoveItem(orderId, "Nuts", 1));
+                commandProcessor.ProcessCommand(new RemoveItem(orderId, "Beer", 6));
                 commandProcessor.ProcessCommand(new AddShipmentAddress(orderId, new Address("Torsmark", "4", "8700", "Horsens")));
 
                 Console.WriteLine("Press ENTER to quit");
