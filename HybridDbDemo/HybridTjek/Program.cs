da94b7a1d0790de6451758117ad671a928734511
Mogens Heller Grabe <mookid8000@gmail.com>
2015-09-16 10:17:42 +0200

    ready for demo

--- HybridDbDemo/HybridTjek/Program.cs
+++ HybridDbDemo/HybridTjek/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using HybridDb;
 using HybridTjek.Model;
 using HybridTjek.Util;
 
@@ -12,98 +11,43 @@ namespace HybridTjek
 
         static void Main()
         {
-            var configurator = new LambdaHybridDbConfigurator(c => { });
+            var orderId = Guid.NewGuid();
+            var order = CreateOrder(orderId.ToString());
 
-            using (var documentStore = DocumentStore.Create(ConnectionString, configurator))
-            {
-                //Random.Next(10).Times(() =>
-                //{
-                //    CreateOrder(documentStore);
-                //});
 
-                //QuerySomeOrders(documentStore);
 
-                Console.WriteLine("Press ENTER to quit");
-                Console.ReadLine();
-            }
+
+
         }
 
-        static void QuerySomeOrders(IDocumentStore documentStore)
+        static Order CreateOrder(string id)
         {
-            const string postalCodeToLookFor = "8700";
-
-            using (var session = documentStore.OpenSession())
-            {
-                var orders = from order in session.Query<Order>()
-                             where order.DeliveryAddress.PostalCode == postalCodeToLookFor
-                             select order;
-
-                Console.WriteLine(@"Found the following orders for {0}:
-{1}", postalCodeToLookFor, string.Join(Environment.NewLine, orders));
-            }
-        }
-
-        static void CreateOrder(IDocumentStore documentStore)
-        {
-            var id = Guid.NewGuid().ToString();
-
             Console.WriteLine("Creating order with ID {0}", id);
 
-            using (var session = documentStore.OpenSession())
+            return new Order
             {
-                var order = new Order
-                {
-                    Id = id,
+                Id = id,
 
-                    OrderLines = Enumerable.Range(0, Random.Next(3) + 1)
-                        .Select(i => new OrderLine
-                        {
-                            ItemName = Random.ItemFrom(ItemNames),
-                            Quantity = Random.Next(5) + 1
-                        })
-                        .ToList(),
+                OrderLines = Enumerable.Range(0, Random.Next(5) + 1)
+                    .Select(i => new OrderLine
+                    {
+                        ItemName = Random.ItemFrom(ItemNames),
+                        Quantity = Random.Next(5) + 1
+                    })
+                    .ToList(),
 
-                    DeliveryAddress = Random.ItemFrom(DeliveryAddresses)
-                };
-
-                session.Store(order);
-
-                session.SaveChanges();
-            }
+                DeliveryAddress = Random.ItemFrom(DeliveryAddresses)
+            };
         }
 
         static readonly string[] ItemNames = { "Beer", "Nuts", "Big TV", "Burger", "Pizza" };
 
         static readonly Address[] DeliveryAddresses =
         {
-            new Address
-            {
-                Street = "Torsmark",
-                HouseNumber = "4",
-                PostalCode = "8700",
-                City = "Horsens"
-            },
-            new Address
-            {
-                Street = "Sdr. Ringgade",
-                HouseNumber = "53",
-                PostalCode = "8000",
-                City = "Aarhus"
-            },
-            new Address
-            {
-                Street = "Ryesgade",
-                HouseNumber = "5",
-                PostalCode = "2200",
-                City = "Nørrebronx"
-            },
-            new Address
-            {
-                Street = "Spobjergvej",
-                HouseNumber = "52",
-                PostalCode = "8220",
-                City = "Brabrand"
-            }
+            new Address {Street = "Torsmark", HouseNumber = "4", PostalCode = "8700", City = "Horsens"},
+            new Address {Street = "Sdr. Ringgade", HouseNumber = "53", PostalCode = "8000", City = "Aarhus"},
+            new Address {Street = "Ryesgade", HouseNumber = "5", PostalCode = "2200", City = "Nørrebronx"},
+            new Address {Street = "Spobjergvej", HouseNumber = "52", PostalCode = "8220", City = "Brabrand"}
         };
 
         static readonly Random Random = new Random(DateTime.Now.GetHashCode());
--- HybridDbDemo/HybridTjek/Util/CustomSerializer.cs
+++ HybridDbDemo/HybridTjek/Util/CustomSerializer.cs
@@ -1,38 +1,37 @@
 ﻿using System;
 using System.Text;
-using HybridDb;
 using Newtonsoft.Json;
 
 namespace HybridTjek.Util
 {
-    public class CustomSerializer : ISerializer
-    {
-        static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-        {
-            TypeNameHandling = TypeNameHandling.All
-        };
+    //public class CustomSerializer : ISerializer
+    //{
+    //    static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+    //    {
+    //        TypeNameHandling = TypeNameHandling.All
+    //    };
 
-        static readonly Encoding TextEncoding = Encoding.UTF8;
+    //    static readonly Encoding TextEncoding = Encoding.UTF8;
 
-        public byte[] Serialize(object obj)
-        {
-            var jsonText = JsonConvert.SerializeObject(obj, Settings);
+    //    public byte[] Serialize(object obj)
+    //    {
+    //        var jsonText = JsonConvert.SerializeObject(obj, Settings);
 
-            return TextEncoding.GetBytes(jsonText);
-        }
+    //        return TextEncoding.GetBytes(jsonText);
+    //    }
 
-        public object Deserialize(byte[] data, Type type)
-        {
-            var jsonText = TextEncoding.GetString(data);
+    //    public object Deserialize(byte[] data, Type type)
+    //    {
+    //        var jsonText = TextEncoding.GetString(data);
 
-            try
-            {
-                return JsonConvert.DeserializeObject(jsonText, Settings);
-            }
-            catch (Exception exception)
-            {
-                throw new JsonSerializationException($"Could not deserialize '{jsonText}'!", exception);
-            }
-        }
-    }
+    //        try
+    //        {
+    //            return JsonConvert.DeserializeObject(jsonText, Settings);
+    //        }
+    //        catch (Exception exception)
+    //        {
+    //            throw new JsonSerializationException($"Could not deserialize '{jsonText}'!", exception);
+    //        }
+    //    }
+    //}
 }
